0439dd75caeea5ae2dec9b1b405a332daaf4c294
TsaoRyan <tsaoryan@foxmail.com>
2017-08-22 09:21:37 +0800

    第三次说爱我

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("wozuiku");
+        System.out.println("那么到我不是最酷的?!");
+        System.out.println("666");
     }
 }
